152393f06ef0908fba1a681d525566dbb5f08040
Shivani <kkrishnashivani18@gmail.com>
2026-04-05 23:42:42 +0530

    added go by example

---
+++ go-learn/go-by-example/values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("2+1", 3+1)
+	fmt.Println("6.0/3.0", 8.0/2.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
